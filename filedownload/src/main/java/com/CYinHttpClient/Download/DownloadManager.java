0111c1825017ea61b3d87e8894b99b97e027de27
Rongxin Sun <sunrongxin7666@163.com>
2017-06-22 23:01:05 +0800

    DownloadThread how to stop thread

--- filedownload/src/main/java/com/CYinHttpClient/Download/DownloadManager.java
+++ filedownload/src/main/java/com/CYinHttpClient/Download/DownloadManager.java
@@ -5,6 +5,7 @@ import com.CYinHttpClient.Download.Http.HttpManager;
 import com.CYinHttpClient.Download.Utils.Logger;
 
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -23,6 +24,7 @@ public class DownloadManager {
 	private static final int MAX_THREAD = 2  ;
 	private static final String TAG = "SRX" ;
 	private static DownloadManager sDownloadManage = new DownloadManager();
+	private HashSet<DownloadTask> mHashSet = new HashSet<>();
 	//线程池
 	private static ThreadPoolExecutor sThreadPool =
 			new ThreadPoolExecutor(MAX_THREAD, MAX_THREAD, 60, TimeUnit.MILLISECONDS,
@@ -45,11 +47,20 @@ public class DownloadManager {
 	private DownloadManager(){
 	}
 
+	public void finsh(DownloadTask task){
+		mHashSet.remove(task);
+	}
 	public void download(final String url, final DownloadCallback callback){
+		final DownloadTask downloadTask = new DownloadTask(url, callback);
+		if(mHashSet.contains(downloadTask)){
+			callback.fail(HttpManager.TASK_RUNNING_ERROR_CODE,"Task is running");
+		}
+		mHashSet.add(downloadTask);
 		HttpManager.getInstance().asyncRequest(url, new Callback() {
 			@Override
 			public void onFailure(Call call, IOException e) {
 				callback.fail(HttpManager.NETWORK_ERROR_CODE,"网络错误");
+				finsh(downloadTask);
 			}
 
 			@Override
@@ -64,6 +75,7 @@ public class DownloadManager {
 					callback.fail(HttpManager.CONTENT_LENGTH_ERROR_CODE,"无法获取网络长度");
 				}
 				processDownload(url,length,callback);
+				finsh(downloadTask);
 			}
 		});
 	}
@@ -72,7 +84,12 @@ public class DownloadManager {
 		long threadDownloadSize = length / MAX_THREAD;
 		for (int i = 0; i < MAX_THREAD ; i++) {
 			long startSize = i*threadDownloadSize;
-			long endSize = (i+1) * threadDownloadSize - 1;
+			long endSize = 0;
+			if (endSize == MAX_THREAD - 1) {
+				endSize = length - 1;
+			} else {
+				endSize = (i + 1) * threadDownloadSize - 1;
+			}
 			sThreadPool.execute(new DownloadRunnable(startSize,endSize,url,callback));
 		}
 	}
--- lib/src/main/java/com/CYinHttpClient/download/ThreadPoolTest.java
+++ lib/src/main/java/com/CYinHttpClient/download/ThreadPoolTest.java
@@ -1,34 +1,67 @@
 package com.CYinHttpClient.download;
 
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.LinkedBlockingDeque;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
-
 /**
  * Created by sunrongxin on 2017/6/21.
  */
 
 public class ThreadPoolTest {
-	public static void main(String args[]){
-		final ArrayBlockingQueue queue = new ArrayBlockingQueue<Runnable>(10);
-		ThreadPoolExecutor threadPoolExecutor =
-				//核心线程数：即初始生成的线程数，
-				new ThreadPoolExecutor(2, 4, 60, TimeUnit.MILLISECONDS, queue);
-
-		for (int i=0; i<20; i++){
-			final int index = i;
-			threadPoolExecutor.execute(new Runnable() {
-				@Override
-				public void run() {
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-					System.out.println("index："+index+" queue size ="+queue.size());
+	static class RunnableToStop implements Runnable{
+		@Override
+		public void run() {
+			//是否被停止或者中断
+			while (flag||!Thread.interrupted()){
+				System.out.println("running");
+				try {
+					Thread.sleep(200);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+					//处理中断异常；
+					System.out.println("Interrupt!");
+					break;
+					//return
 				}
-			});
+			}
+			System.out.println("Stop!");
 		}
+
+		public void stop() {
+			flag = false;
+		}
+
+		public void stopWithInterrupt(Thread thread){
+			thread.interrupt();
+			stop();
+		}
+
+		private volatile Boolean flag = true;
 	}
+	public static void main(String args[]) throws InterruptedException {
+//		final LinkedBlockingDeque queue = new LinkedBlockingDeque<Runnable>(10);
+//		ThreadPoolExecutor threadPoolExecutor =
+//				//核心线程数：即初始生成的线程数，
+//				new ThreadPoolExecutor(2, 4, 60, TimeUnit.MILLISECONDS, queue);
+//
+//		for (int i=0; i<20; i++){
+//			final int index = i;
+//			threadPoolExecutor.execute(new Runnable() {
+//				@Override
+//				public void run() {
+//					try {
+//						Thread.sleep(1000);
+//					} catch (InterruptedException e) {
+//						e.printStackTrace();
+//					}
+//					System.out.println("index："+index+" queue size ="+queue.size());
+//				}
+//			});
+//		}
+		RunnableToStop runnable = new RunnableToStop();
+
+		Thread thread = new Thread(runnable);
+		thread.start();
+		Thread.sleep(1000);
+		runnable.stopWithInterrupt(thread);
+	}
+
+
 }
