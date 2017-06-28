589d463b59046974b316823ed6f496ea7f3f2282
Rongxin Sun <sunrongxin7666@163.com>
2017-06-28 23:37:54 +0800

    HttpFramework

---
+++ httpframework/src/main/java/com/android/srx/github/httpframework/HttpMethod.java
@@ -0,0 +1,9 @@
+package com.android.srx.github.httpframework;
+
+/**
+ * Created by sunrongxin on 2017/6/28.
+ */
+
+public enum HttpMethod {
+	GET, POST, TRACE, PUT, DELETE, CONNECTION, OPTIONS
+}
---
+++ httpframework/src/main/java/com/android/srx/github/httpframework/NameValueMap.java
@@ -0,0 +1,13 @@
+package com.android.srx.github.httpframework;
+
+import java.util.Map;
+
+
+interface NameValueMap extends Map<String,String> {
+
+	String get(String key);
+
+	void set(String key, String value);
+
+	void setAll(Map<String,String> map);
+}
