6582892ee80305e5e5b156460aa868ff33f51d7e
Rongxin Sun <sunrongxin7666@163.com>
2017-07-05 00:14:18 +0800

    HttpFramework 2

---
+++ httpframework/src/main/java/com/android/srx/github/httpframework/Header.java
@@ -0,0 +1,9 @@
+package com.android.srx.github.httpframework;
+
+/**
+ * Created by sunrongxin on 2017/7/5.
+ */
+
+public interface Header {
+	HttpHeader getHeaders();
+}
---
+++ httpframework/src/main/java/com/android/srx/github/httpframework/HttpResponse.java
@@ -0,0 +1,23 @@
+package com.android.srx.github.httpframework;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by sunrongxin on 2017/7/5.
+ */
+
+public interface HttpResponse extends Header, Closeable {
+
+	HttpStatus getStatus();
+
+	String getStatusMsg();
+
+	InputStream getBody() throws IOException;
+
+	void close();
+
+	long getContentLength();
+
+}
